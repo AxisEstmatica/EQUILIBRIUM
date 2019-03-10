07cfeebc33295983ef2c8138ec6c4493a3752f1d
Anistrix <47559282+Anistrix@users.noreply.github.com>
2019-03-10 14:01:56 +0300

    fix
    Co-Authored-By: Anistrix <axisestmatica@users.noreply.github.com>

--- Laboratornaya 4/Laboratornaya 4/Laboratornaya 4/main.c
+++ Laboratornaya 4/Laboratornaya 4/Laboratornaya 4/main.c
@@ -1,81 +1,93 @@
-//#pragma warning(disable : 4996) // отключение ошибки c4996
+#pragma warning(disable : 4996) // отключение ошибки c4996
 #include <stdio.h>
 //
 #define YES 1
 #define NO 0
 #define MAXLINE 1000
 //
-void process_line(char buffer[]); //
+void process_line(char buffer[]); // объ€вл€ем функцию
 
 int main(void) //
 {
 	char line[MAXLINE]; //
 	//
 	printf("Please, enter some words,\nWARNING: words that start and end on similar letter will be deleted\n");
-	gets(line);  //
-	process_line(line); //
+	gets(line);  // считываем строку до конца файла и запихиваем в массив line
+	process_line(line); // выполн€ем функцию над массивом line
 	printf("Result:\n");
-	puts(line); //
+	puts(line); // выводим массив line
 	return 0;
 }
 
-void process_line(char buffer[]) //
+void process_line(char buffer[]) // тело функции
 {
-	char c;  // текущий символ
+	int c;  // текущий символ
 	int prev_c; // предыдущий символ
 	int flag; // признак разделени€ слов
 	int C; // первый символ
 	int i; // позици€ символа
 	int post_i; // позици€ символа в финальном варианте
 	int j; // позици€ начала слова
+	int found; // признак отсутстви€ слова
 
 	// ну понеслаааась
+	found = NO;
 	flag = NO;
-	C = ' ';
+	C = buffer[0]; // присваеваем первый символ, чтобы не потер€ть его
 	j = 0;
-	prev_c = ' '; //
+	prev_c = buffer[0]; // та же причина
 	i = 0; // потому что массивы начинаютс€ с нул€
-	post_i = 0; // та же причина
+	post_i = -1; // особенноесть for
 
 	// читаем строчки
 
 	do // цикл с постусловием
 	{
 		c = buffer[i];
-		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0') //
+		if (((c < 'a' && c < 'z') && (c < 'A' && c < 'Z')) && (c != ' ' && c != '.' && c != ',' && c != '\n' && c != '\0' && c != '\t')) // проверка на не слово
 		{
-			flag = YES;
-			if (prev_c == C) //
+			found = YES;
+		}
+		if (c == ' ' || c == '.' || c == ',' || c == '\n' || c == '\0' || c == '\t') // проверка конец слова
+		{
+			flag = YES; // нашли разделитель
+			if ((prev_c - 'A' == C - 'A' || prev_c - 'a' == C - 'a' || prev_c - 'a' == C - 'A' || prev_c - 'A' == C - 'a') && found != YES) // проверка на условие
 			{
-
+				
 			}
 			else
 			{
-				for (; j < i; ++j) //
+				if (buffer[i-1] == ' ' || buffer[i-1] == '.' || buffer[i-1] == ',' || buffer[i-1] == '\n' || buffer[i-1] == '\0' || buffer[i-1] == '\t') // если предыдущий символ разделитель, то...
 				{
-					buffer[post_i] = buffer[j]; //
 					++post_i;
+					buffer[post_i] = c; // просто ставим разделитель
 				}
-				buffer[post_i] = c; //
-				++post_i;
+				else
+				{
+					for (++post_i; j < i; ++j) // перезапись слова в тот-же массив
+					{
+						buffer[post_i] = buffer[j]; // присвоение
+						++post_i;
+					}
+					buffer[post_i] = c; // устанавливаем разделитель между словами
+				}
 			}
+			found = NO;
 
 		}
 		else
 		{
-			if (flag == YES) //
+			if (flag == YES) // проверка на прошедший разделитель
 			{
-				C = c; //
-				flag = NO; //
-				j = i; //
+				C = c; // первый символ не разделитель
+				flag = NO; // обнул€ем флаг
+				j = i; // запоминаем место первого символа
 			}
-			prev_c = c; //
+			prev_c = c; // запоминаем последний не разделитель
 		}
 		++i;
 
-	} while (c != '\0'); //
-	for (; post_i < MAXLINE; ++post_i) // очищение строки от остаточных символов
-	{
-		buffer[post_i] = 0;
-	}
+	} 
+	while (c != '\0'); // цикл работает до конца файла
+	buffer[post_i] = '\0';// устанавливаем конец файла
 }
