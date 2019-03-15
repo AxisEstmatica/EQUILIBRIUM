ed80ce572288f58093134d7a671117a75312a850
Anistrix <47569016+AxisEstmatica@users.noreply.github.com>
2019-03-15 10:23:11 +0300

    tsssssssssssss

--- Laboratornaya 5/main.c
+++ Laboratornaya 5/main.c
@@ -1,3 +1,17 @@
-﻿//#pragma warning(disable : 4996) // отключение ошибки c4996
+#pragma warning(disable : 4996) // отключение ошибки c4996
 #include <stdio.h>
 
+#define N 10
+
+int main(void)
+{
+  char MAS[N];
+  
+  for (i = 0; i < N; ++i)
+  {
+    printf("Number %d, i+1") // ne znaiy kak budet rabotat'
+    scanf("%d", &MAS[i]);
+  }
+  
+return 0;
+}
