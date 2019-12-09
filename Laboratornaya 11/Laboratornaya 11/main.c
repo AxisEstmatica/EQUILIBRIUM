fe472f22041b9e1279d6de35cd8b980d6f7813a9
AxisEstmatica <zerkov.axsis@gmail.com>
2019-12-09 06:22:17 +0300

    optimisation
    he-he
    
    Co-Authored-By: Anistrix <anistrix@users.noreply.github.com>
    Co-Authored-By: Anistrix <axisestmatica@users.noreply.github.com>

--- Laboratornaya 11/Laboratornaya 11/main.c
+++ Laboratornaya 11/Laboratornaya 11/main.c
@@ -1,42 +1,20 @@
 #pragma warning(disable : 4996) // отключение ошибки c4996
 #include <stdio.h>
-//#include <malloc.h>
-//#include <string.h>
-//#include <stdlib.h>
 #include <windows.h>
-#define YES 1
-#define NO 0
+#include <locale.h>
 #define STR 1024
 
-
-
-//void read(char *ptr);
 void processing(char num[STR / 8]);
 
 void main(void)
 {
+	char *locale = setlocale(LC_ALL, "");
 	WORD foregroundColor0; // цвет слов
 	WORD textAttribute;	   // атрибут текста - цвет символа и фона
 	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-	foregroundColor0 = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN /*| FOREGROUND_RED*/;
-	textAttribute = foregroundColor0;// | backgroundColor;
+	foregroundColor0 = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN;
+	textAttribute = foregroundColor0;
 	SetConsoleTextAttribute(hStdout, textAttribute);
-	/*
-	WORD foregroundColor0; // цвет слов  
-	WORD foregroundColor1; // цвет разделителей  
-	//WORD foregroundColor;  // цвет символа  
-	WORD backgroundColor;  // цвет фона  
-	WORD textAttribute;	   // атрибут текста - цвет символа и фона 
-
- // Получить стандартный дескриптор   
-	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-
- // определить цвета символов, разделителей и фона  
-	foregroundColor0 = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN /*| FOREGROUND_RED*/;
-	//foregroundColor1 = FOREGROUND_INTENSITY | FOREGROUND_RED;
-	//backgroundColor = BACKGROUND_INTENSITY | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED;
-	
-
 	int i; // счётчик
 	int j; // счётчик
 	char str[STR]; // массив строки
@@ -57,7 +35,7 @@ void main(void)
 	printf("Path of file: %s\n", str); // отладка
 	frd = fopen((str), "rt");
 	if (frd == NULL)   
-		return; // chk open file
+		return -2; // chk open file
 
 	while (!feof(frd)) // for EOF
 	{
@@ -93,51 +71,31 @@ void main(void)
 			++i;
 		}
 	}
-	
-
-
-
-
-
-
-
 	fclose(frd);
 	return 0;
 }
 
 void processing(char num[STR / 8])
 {
-	WORD foregroundColor0; // цвет слов  
-	WORD foregroundColor1; // цвет разделителей  
-	//WORD foregroundColor;  // цвет символа  
-	WORD backgroundColor;  // цвет фона  
-	WORD textAttribute;	   // атрибут текста - цвет символа и фона 
-
-	// Получить стандартный дескриптор   
-	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	// определить цвета символов, разделителей и фона  
-	foregroundColor0 = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN /*| FOREGROUND_RED*/;
+	WORD foregroundColor0; // цвет обычный
+	WORD foregroundColor1; // цвет необычный 
+	WORD textAttribute;	   // атрибут текста - цвет символа и фона  
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+	foregroundColor0 = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN;
 	foregroundColor1 = FOREGROUND_INTENSITY | FOREGROUND_RED;
-	backgroundColor = BACKGROUND_INTENSITY | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED;
-
 	int j = 0;
 	int value = strlen(num);
 	while (num[j] == '-' || (num[j] >= '0'&& num[j] <= '9'))
-	{
 		++j;
-	}
 	if (value == j)
 	{
-		textAttribute = foregroundColor1; //| backgroundColor;
+		textAttribute = foregroundColor1;
 		SetConsoleTextAttribute(hStdout,textAttribute);
 		printf("%s", num);
-		textAttribute = foregroundColor0;// | backgroundColor;
+		textAttribute = foregroundColor0;
 		SetConsoleTextAttribute(hStdout, textAttribute);
 	}
 	else
-	{
 		printf("%s", num);
-	}
 	return;
 }
